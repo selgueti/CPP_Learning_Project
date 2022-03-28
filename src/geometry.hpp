efe18ff42eb40ab570e9ceb8ca377d79377f1b5f
selgueti <sofian.elguetibi@gmail.com>
2022-03-28 21:44:48 +0200

    Task 2.1.C done

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <numeric>
 
 struct Point2D
 {
@@ -64,7 +65,7 @@ struct Point2D
 
 struct Point3D
 {
-    float values[3] {};
+    std::array<float, 3> values {};
 
     Point3D() {}
     Point3D(float x, float y, float z) : values { x, y, z } {}
@@ -80,25 +81,24 @@ struct Point3D
 
     Point3D& operator+=(const Point3D& other)
     {
-        x() += other.x();
-        y() += other.y();
-        z() += other.z();
+
+        std::transform(values.begin(), values.end(), other.values.begin(), values.begin(),
+                       std::plus<float>());
+
         return *this;
     }
 
     Point3D& operator-=(const Point3D& other)
     {
-        x() -= other.x();
-        y() -= other.y();
-        z() -= other.z();
+        std::transform(values.begin(), values.end(), other.values.begin(), values.begin(),
+                       std::minus<float>());
         return *this;
     }
 
     Point3D& operator*=(const float scalar)
     {
-        x() *= scalar;
-        y() *= scalar;
-        z() *= scalar;
+        std::transform(values.begin(), values.end(), values.begin(),
+                       [scalar](float v) { return v * scalar; });
         return *this;
     }
 
@@ -125,8 +125,12 @@ struct Point3D
 
     Point3D operator-() const { return Point3D { -x(), -y(), -z() }; }
 
-    float length() const { return std::sqrt(x() * x() + y() * y() + z() * z()); }
-
+    // float length() const { return std::sqrt(x() * x() + y() * y() + z() * z()); }
+    float length() const
+    {
+        return std::sqrt(std::reduce(values.begin(), values.end(), 0.0,
+                                     [](float acc, float cur) { return acc + (cur * cur); }));
+    }
     float distance_to(const Point3D& other) const { return (*this - other).length(); }
 
     Point3D& normalize(const float target_len = 1.0f)
